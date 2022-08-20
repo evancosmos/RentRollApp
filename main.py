6e121da1a15f4b5e8638dfd2a03fb6ad247458f3
Evan Cosmos <evanclark@live.ca>
2022-08-20 14:32:44 -0700

    Added some communication with firebase database

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 #Read text from PDFs
 from pdfminer.high_level import extract_text
+
 #Read text from images https://www.geeksforgeeks.org/how-to-extract-text-from-images-with-python/
 from PIL import Image
-from pytesseract import pytesseract #Requires the tesseract executable on the system.
-#https://linuxhint.com/install-tesseract-ocr-linux/
+from pytesseract import pytesseract #Requires the tesseract executable on the system. https://linuxhint.com/install-tesseract-ocr-linux/
 
-#TODO: Fix duplicated print, implment ODR
+#Hosting Service
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-class rentRollEntity:
+cred = credentials.Certificate("./firebasekeys.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://rent-roll-webapp-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('/Test12')
+print(ref.get())
+
+#Web App framework for python
+from flask import Flask, request
+
+#FLASK START
+app = Flask(__name__)
+    
+@app.route("/", methods=['GET', 'POST'])
+def helloworld():
+    if request.method == 'POST':
+        f = request.files['the_file'] #Get the file, send it to database
+
+    return "<p>Hello World<p> <button>Upload</button> <button>Upload</button>"
+#FLASK END
+
+#TODO: Train ODR, Set up database connections, Set up frontend connections, 
+
+class rentRollEntity: #Individual Items on a Rent Roll
     def __init__(self):
         self.leaseNum = ""
         self.operatingName = ""
@@ -21,7 +48,7 @@ class rentRollEntity:
     def printRoll(self):
         print("Lease number: " + self.leaseNum + "\n Operating Name: " + self.operatingName + "\n Square Feet: " + self.sqrFt + "\n Status: " + self.status + "\n Renewal Option: " + self.renOption + "\n Lease Expires " + self.leaseExpire + "\n Renewal Notice: " + self.renNotice + "\n Orig Com: " + self.origCom + "\n\n")
 
-class rentRolls:
+class rentRolls: #The collection of items on a rent roll
     def __init__(self) -> None:
         self.rolls = []
 
@@ -41,7 +68,7 @@ def readImage(filename):
     allRoll = rentRolls()
     return allRoll
 
-def readPDFTemplate1(filename):
+def readPDFTemplate1(filename): #For this template, a new item is begins when an line starts with "TCC"
     text = extract_text(filename)
     textLines = text.splitlines()
     #Once "TCC" is read from the start of a line, make a new roll
@@ -85,7 +112,8 @@ def readPDFTemplate1(filename):
     return allRoll
 
 def main():
-    readImage('1650 lease rent roll.png')
+    pass
+    #readImage('1650 lease rent roll.png')
     #readPDFTemplate1('2018-05-16 - Tamarack - Base Rent Roll.pdf')
 
 if __name__ == "__main__":
