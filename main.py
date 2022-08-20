48715e0de10363bf1bda514c72f6599c350e045c
Evan Cosmos <evanclark@live.ca>
2022-08-20 15:19:54 -0700

    JSONfiyed the output

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #Read text from PDFs
 from pdfminer.high_level import extract_text
 
+#Output to site/database with json
+import json
+
 #Read text from images https://www.geeksforgeeks.org/how-to-extract-text-from-images-with-python/
 from PIL import Image
 from pytesseract import pytesseract #Requires the tesseract executable on the system. https://linuxhint.com/install-tesseract-ocr-linux/
@@ -10,16 +13,18 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("./firebasekeys.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://rent-roll-webapp-default-rtdb.firebaseio.com/"
-})
-
-ref = db.reference('/Test12')
-print(ref.get())
-
 #Web App framework for python
 from flask import Flask, request
+
+#Firebase START
+def firebaseConnect():
+    cred = credentials.Certificate("./firebasekeys.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': "https://rent-roll-webapp-default-rtdb.firebaseio.com/"
+    })
+
+    ref = db.reference('/Test12')
+    print(ref.get())
 
 #FLASK START
 app = Flask(__name__)
@@ -32,7 +37,7 @@ def helloworld():
     return "<p>Hello World<p> <button>Upload</button> <button>Upload</button>"
 #FLASK END
 
-#TODO: Train ODR, Set up database connections, Set up frontend connections, 
+#TODO: Train ODR, Set up database connections, Set up frontend connections.
 
 class rentRollEntity: #Individual Items on a Rent Roll
     def __init__(self):
@@ -45,36 +50,51 @@ class rentRollEntity: #Individual Items on a Rent Roll
         self.renNotice = ""
         self.origCom = ""
 
-    def printRoll(self):
-        print("Lease number: " + self.leaseNum + "\n Operating Name: " + self.operatingName + "\n Square Feet: " + self.sqrFt + "\n Status: " + self.status + "\n Renewal Option: " + self.renOption + "\n Lease Expires " + self.leaseExpire + "\n Renewal Notice: " + self.renNotice + "\n Orig Com: " + self.origCom + "\n\n")
+    def retAsDict(self):
+        newDict = {
+            "Lease": self.leaseNum,
+            "Operating Name": self.operatingName,
+            "Square Feet": self.sqrFt,
+            "Status": self.status,
+            "Renewal Option": self.renOption,
+            "Lease Expires": self.leaseExpire,
+            "Renewal Notice": self.renNotice,
+            "Origination Com": self.origCom
+        }
+        return newDict
 
-class rentRolls: #The collection of items on a rent roll
+class rentRoll: #The collection of items on a rent roll
     def __init__(self) -> None:
         self.rolls = []
 
     def addRoll(self, newRoll):
         self.rolls.append(newRoll)
 
-    def listRolls(self):
+    def retMasterDict(self):
+        newMasterDict = {}
+        id = 0
         for roll in self.rolls:
-            roll.printRoll()
-
+            newMasterDict[str(id)] = roll.retAsDict()
+            id += 1
+        return newMasterDict
+            
 def readImage(filename):
     img = Image.open(filename)
 
     text = pytesseract.image_to_string(img)
 
     print(text)
-    allRoll = rentRolls()
+    allRoll = rentRoll()
     return allRoll
 
 def readPDFTemplate1(filename): #For this template, a new item is begins when an line starts with "TCC"
     text = extract_text(filename)
     textLines = text.splitlines()
     #Once "TCC" is read from the start of a line, make a new roll
-    allRoll = rentRolls()
+    allRoll = rentRoll()
     itemCount = 0
-    for line in textLines[:500]:
+    for line in textLines[:500]: #Remember to remove the :500 limiter after testing
+        #print(line)
         if(line == ''):
             itemCount = itemCount - 1
         elif(line[0:3] == "TCC"): #A new item is made
@@ -108,13 +128,21 @@ def readPDFTemplate1(filename): #For this template, a new item is begins when an
             itemCount = itemCount - 1
         itemCount += 1
 
-    allRoll.listRolls()
     return allRoll
 
+def rollToJSON(rentRoll):
+
+    x = rentRoll.retMasterDict()
+
+    with open('out.json', 'w', encoding='utf-8') as f:
+        json.dump(x, f, ensure_ascii=False, indent=4)
+
+    return #return json file for general purpose later
+
 def main():
-    pass
     #readImage('1650 lease rent roll.png')
-    #readPDFTemplate1('2018-05-16 - Tamarack - Base Rent Roll.pdf')
+    gotRoll = readPDFTemplate1('2018-05-16 - Tamarack - Base Rent Roll.pdf')
+    rollToJSON(gotRoll)
 
 if __name__ == "__main__":
     main()
